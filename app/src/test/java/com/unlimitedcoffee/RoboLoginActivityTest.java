360eb2b725c3b48f9ce5caa9ec868b85fd2a9f21
murk <emurken@gmail.com>
2019-06-23 17:32:02 -0500

    Robolectric testing template - in progress

---
+++ app/src/test/java/com/unlimitedcoffee/RoboLoginActivityTest.java
@@ -0,0 +1,41 @@
+package com.unlimitedcoffee;
+
+// Static imports for assertion methods
+import android.os.Build;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricTestRunner.class)
+
+public class RoboLoginActivityTest {
+
+    // @Before => JUnit 4 annotation that specifies this method should run before each test is run
+    // Useful to do setup for objects that are needed in the test
+    @Before
+    public void setup() {
+        // Convenience method to run MainActivity through the Activity Lifecycle methods:
+        // onCreate(...) => onStart() => onPostCreate(...) => onResume()
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    // @Test => JUnit 4 annotation specifying this is a test to be run
+    // The test simply checks that our TextView exists and has the text "Hello world!"
+    @Test
+    public void validateTextViewContent() {
+        TextView tvHelloWorld = (TextView) activity.findViewById(R.id.tvHelloWorld);
+        assertNotNull("TextView could not be found", tvHelloWorld);
+        assertTrue("TextView contains incorrect text",
+                "Hello world!".equals(tvHelloWorld.getText().toString()));
+    }
+
+
+
+}
