f02e7fdff0d99e6aeb73d3055c99f513221f2e8d
murk <emurken@gmail.com>
2019-06-23 17:32:34 -0500

    LoginActivity JUnit test file - in progress

---
+++ app/src/test/java/com/unlimitedcoffee/LoginActivityUnitTest.java
@@ -0,0 +1,22 @@
+package com.unlimitedcoffee;
+
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class LoginActivityUnitTest {
+
+    @Test
+    public void testOnBackPressed()  {
+
+    }
+    @Test
+    public void testOnCreate() {
+
+    }
+
+}
